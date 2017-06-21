0d0d9b27af5c19cd2eb915c2a62a4c7d9f745fb8
hehai <gzhehai@foxmail.com>
2017-06-21 12:33:07 +0800

    Added simple serialization of  json & xml

---
+++ src/OhDotNetLib/Serialization/JsonSerializer.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+
+namespace OhDotNetLib.Serialization
+{
+    public class JsonSerializer
+    {
+        public static string Serializer(object obj)
+        {
+            return Newtonsoft.Json.JsonConvert.SerializeObject(obj);
+        }
+
+        public static TObject Deserializer<TObject>(string json)
+        {
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<TObject>(json);
+        }
+    }
+}
---
+++ src/OhDotNetLib/Serialization/XmlSerializer.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+
+using SystemXmlSerializer = System.Xml.Serialization.XmlSerializer;
+
+namespace OhDotNetLib.Serialization
+{
+    public class XmlSerializer
+    {
+        public static string Serializer(object obj)
+        {
+            var _str = string.Empty;
+            var xmlserializer = new SystemXmlSerializer(obj.GetType());
+            using (var stream = new MemoryStream())
+            {
+                xmlserializer.Serialize(stream, obj);
+                using (var reader = new StreamReader(stream))
+                {
+                    stream.Position = 0;
+                    _str = reader.ReadToEnd();
+                }
+            }
+            return _str;
+        }
+
+        public static TObject Deserializer<TObject>(string xml)
+        {
+            var _obj = default(TObject);
+            var xmlserializer = new SystemXmlSerializer(typeof(TObject));
+            using (var stream = new MemoryStream())
+            {
+                using (var writer = new StreamWriter(stream))
+                {
+                    writer.Write(xml);
+                    writer.Flush();
+                    stream.Position = 0;
+                    _obj = (TObject)xmlserializer.Deserialize(stream);
+                }
+            }
+            return _obj;
+        }
+    }
+}
