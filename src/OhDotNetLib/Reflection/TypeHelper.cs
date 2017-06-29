59d4a67a34fa58664924ddbd3eafd997803cc903
hehai <gzhehai@foxmail.com>
2017-06-29 20:21:58 +0800

    add GetGenericTypeUniqueName() api

--- src/OhDotNetLib/Reflection/TypeHelper.cs
+++ src/OhDotNetLib/Reflection/TypeHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Text;
 
@@ -30,6 +31,25 @@ namespace OhDotNetLib.Reflection
         public static bool IsGenericTypeDefinition(Type type)
         {
             return type.GetTypeInfo().IsGenericTypeDefinition;
+        }
+        #endregion
+
+        #region GetGenericTypeUniqueName
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static string GetGenericTypeUniqueName(Type type)
+        {
+            var name = type.FullName;
+            if (TypeHelper.IsGenericType(type))
+            {
+                name = name.Substring(0, name.IndexOf("`") + 1);
+                name = $"{name}{type.GetTypeInfo().GetGenericArguments().Count()}";
+            }
+            return name;
         }
         #endregion
     }
--- test/OhDotNetLib.Tests/Reflection/TypeHelperTest.cs
+++ test/OhDotNetLib.Tests/Reflection/TypeHelperTest.cs
@@ -1,10 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
+using Shouldly;
+using OhDotNetLib.Reflection;
 
 namespace OhDotNetLib.Tests.Reflection
 {
-    class TypeHelperTest
+    public class TypeHelperTest
     {
+        #region Verify_GetGenericTypeUniqueNameShouldBeWork
+
+        [Fact]
+        public void Verify_GetGenericTypeUniqueNameShouldBeWork()
+        {
+            var name = TypeHelper.GetGenericTypeUniqueName(typeof(Int32));
+            var name1 = TypeHelper.GetGenericTypeUniqueName(typeof(MyGenericTypeTestClass1<>));
+            var name2 = TypeHelper.GetGenericTypeUniqueName(typeof(MyGenericTypeTestClass1<,>));
+            var name3 = TypeHelper.GetGenericTypeUniqueName(typeof(MyGenericTypeTestClass1<,,,,,,,,,,>));
+            var name4 = TypeHelper.GetGenericTypeUniqueName(typeof(MyGenericTypeTestClass1<,,,,,,,,,,>));
+
+            Assert.NotSame(name1, name2);
+            Assert.NotSame(name1, name3);
+            Assert.NotSame(name2, name3);
+
+             Assert.Same(name4, name4);
+        }
+        #endregion
+    }
+
+    internal class MyGenericTypeTestClass1<TTpye1>
+    {
+
+    }
+
+    internal class MyGenericTypeTestClass1<TTpye1, TTpye2>
+    {
+
+    }
+
+    internal class MyGenericTypeTestClass1<TTpye1, TTpye2, TTpye3>
+    {
+
+    }
+
+    internal class MyGenericTypeTestClass1<TTpye1, TTpye2, TTpye3, TTpye4>
+    {
+
+    }
+    internal class MyGenericTypeTestClass1<TTpye1, TTpye2, TTpye3, TTpye4, TTpye5, TTpye6, TTpye7, TTpye8, TTpye9, TTpye10, TTpye11>
+    {
+
     }
 }
