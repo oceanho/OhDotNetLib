bc0b2d2fba80be13e0ddb0a4653a0e3043663ba3
hehai <gzhehai@foxmail.com>
2017-07-02 12:43:45 +0800

    add UnixTimeStamp Conversion api

--- src/OhDotNetLib/Extension/DateTimeOfSelfConversion/DateTimeOfSelfConversionExtension.cs
+++ src/OhDotNetLib/Extension/DateTimeOfSelfConversion/DateTimeOfSelfConversionExtension.cs
@@ -4,11 +4,12 @@ using System.Text;
 using System.Linq;
 
 namespace OhDotNetLib.Extension
-{    
+{
     public static partial class DateTimeOfSelfConversionExtension
     {
+        private const long unix_ts_const = 621355968000000000;
         /// <summary>
-        /// 获取当前指定时间的当天的最小时间（返回日期 <paramref name="source"/> 当天 00:00:00 时刻的时间）
+        /// 获取指定时间的当天的最小时间（返回日期 <paramref name="source"/> 当天 00:00:00 时刻的时间）
         /// </summary>
         /// <param name="source"></param>
         /// <returns></returns>
@@ -18,13 +19,65 @@ namespace OhDotNetLib.Extension
         }
 
         /// <summary>
-        /// 获取当前指定时间的当天的最大时间（返回日期 <paramref name="source"/> 当天 23:59:59 时刻的时间）
+        /// 获取指定时间的当天的最大时间（返回日期 <paramref name="source"/> 当天 23:59:59 时刻的时间）
         /// </summary>
         /// <param name="source"></param>
         /// <returns></returns>
         public static DateTime GetMaxOfDay(this DateTime source)
         {
             return new DateTime(source.Year, source.Month, source.Day, 23, 59, 59);
+        }
+
+        /// <summary>
+        /// 获取指定时间的 Unix TimeStamp
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static long GetUnixTimeStamp(this DateTime source)
+        {
+            return (long)((source.ToUniversalTime().Ticks - unix_ts_const) / 10000000);
+        }
+
+        /// <summary>
+        /// 获取指定的 Unix TimeStamp Utc 时间
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static DateTime ToUtcDateTime(this Int32 source)
+        {
+            return new DateTime((source * 10000000 + unix_ts_const), DateTimeKind.Utc);
+        }
+
+
+
+        /// <summary>
+        /// 获取指定的 Unix TimeStamp Utc 时间
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static DateTime ToUtcDateTime(this Int64 source)
+        {
+            return new DateTime((source * 10000000 + unix_ts_const), DateTimeKind.Utc);
+        }
+
+        /// <summary>
+        /// 获取指定的 Unix TimeStamp Local 时间
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static DateTime ToLocalDateTime(this Int32 source)
+        {
+            return source.ToUtcDateTime().ToLocalTime();
+        }
+
+        /// <summary>
+        /// 获取指定的 Unix TimeStamp Local 时间
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static DateTime ToLocalDateTime(this Int64 source)
+        {
+            return source.ToUtcDateTime().ToLocalTime();
         }
     }
 }
---
+++ test/OhDotNetLib.Tests/Extension/DateTimeOfSelfConversion/DateTimeOfSelfConversionExtensionTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Xunit;
+using Shouldly;
+
+namespace OhDotNetLib.Tests.Extension
+{
+    using OhDotNetLib.Extension;
+
+    public class DateTimeOfSelfConversionExtensionTest
+    {
+        #region Verify_DateTimeConversionShouldBeWork
+
+        [Fact]
+        public void Verify_DateTimeConversionShouldBeWork()
+        {
+            var date = DateTime.Now.GetMinOfDay();
+            var ts = date.GetUnixTimeStamp();
+            ts.ToLocalDateTime().ShouldBe(date);
+            ts.ToUtcDateTime().ShouldBe(date.ToUniversalTime());
+        }
+        #endregion
+    }
+}
